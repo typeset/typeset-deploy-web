6cdccb5e21b9c83a3fd9c06b02115c4fa1dda984
Bates Westmoreland <rbwestmoreland@gmail.com>
2012-07-21 17:29:52 -0400

    Added Twitter Bootstrap and AppHarbor Deployment
    Added Twitter Bootstrap
    Added additional markup for Index
    Added a shared Header
    Added DeployController and views

---
+++ Typeset.Deploy.Web/Controllers/DeployController.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using Typeset.Deploy.Web.Models.Deploy;
+
+namespace Typeset.Deploy.Web.Controllers
+{
+    public class DeployController : Controller
+    {
+        [HttpGet]
+        public ActionResult AppHarbor()
+        {
+            return View(new AppHarbor());
+        }
+
+        [HttpPost]
+        public ActionResult AppHarbor(AppHarbor model)
+        {
+            if (ModelState.IsValid)
+            {
+                var success = false;
+
+                using (var client = new HttpClient())
+                {
+                    var json = new
+                    {
+                        branches = new
+                        {
+                            @default = new
+                            {
+                                commit_id = "typeset-vLatest", //todo: Provide appropriate id
+                                commit_message = "trigger via typeset-deploy",
+                                download_url = "https://github.com/typeset/typeset/tarball/master",
+                            }
+                        }
+                    };
+
+                    var content = new StringContent(JsonConvert.SerializeObject(json));
+                    var response = client.PostAsync(model.BuildUrl, content).Result;
+                    success = response.StatusCode.Equals(HttpStatusCode.OK);
+                }
+
+                return View("Success");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+    }
+}
---
+++ Typeset.Deploy.Web/Models/Deploy/AppHarbor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Typeset.Deploy.Web.Models.Deploy
+{
+    public class AppHarbor
+    {
+        [Required]
+        [Display(Name = "build url")]
+        public string BuildUrl { get; set; }
+    }
+}
