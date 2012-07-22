b64bfcedff74507f703c10a95318805b4d0c1a37
Bates Westmoreland <rbwestmoreland@gmail.com>
2012-07-22 13:40:41 -0400

    Added IGitHubRepository/GitHubRepository
    Added IGitHubRepository/GitHubRepository with ability to get the latest
    commit download url and sha.

---
+++ Typeset.Deploy.Domain/Repository/GitHubRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace Typeset.Deploy.Domain.Repository
+{
+    public class GitHubRepository : IGitHubRepository
+    {
+        private const string GitHubApiUrl = "https://api.github.com";
+
+        public string GetLatestCommitDownloadUrl(string username, string repositoryName)
+        {
+            var latestCommitId = GetLatestCommitSha(username, repositoryName);
+            var downloadUrl = string.Format("https://github.com/{0}/{1}/tarball/{2}", username, repositoryName, latestCommitId);
+            return downloadUrl;
+        }
+
+        public string GetLatestCommitSha(string username, string repositoryName)
+        {
+            var sha = string.Empty;
+
+            using (var client = new HttpClient())
+            {
+                var requestUrl = string.Format("{0}/repos/{1}/{2}/commits", GitHubApiUrl, username, repositoryName);
+                var response = client.GetAsync(requestUrl).Result;
+                var jsonString = response.Content.ReadAsStringAsync().Result;
+                dynamic json = JsonConvert.DeserializeObject(jsonString);
+                dynamic latestCommit = json[0];
+                sha = latestCommit.sha.Value.ToString();
+            }
+
+            return sha;
+        }
+    }
+}
---
+++ Typeset.Deploy.Domain/Repository/IGitHubRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Typeset.Deploy.Domain.Repository
+{
+    public interface IGitHubRepository
+    {
+        string GetLatestCommitDownloadUrl(string username, string repositoryName);
+        string GetLatestCommitSha(string username, string repositoryName);
+    }
+}
