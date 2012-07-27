1e6f6bbf09a7bb60163b79097a9b74dd56b10f98
Bates Westmoreland <rbwestmoreland@gmail.com>
2012-07-27 19:16:20 -0400

    Added HtmlHelpers & Version Meta to Master Page
    Added HtmlHelpers
    Added version meta tag to Master Page

---
+++ Typeset.Deploy.Web/Views/Helpers/HtmlHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace System.Web.Mvc
+{
+    public static class HtmlHelpers
+    {
+        private static string _ApplicationVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+        public static string ApplicationVersion(this HtmlHelper helper)
+        {
+            return _ApplicationVersion;
+        }
+    }
+}
